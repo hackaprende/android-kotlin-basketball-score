f16ea04a77542348abc7b4011c17ec564fc2bd2f
Jesus Almaral <almaral17@gmail.com>
2020-08-01 16:39:54 -0500

    Add functionality to the app

--- app/src/main/java/com/hackaprende/basketballscore/MainActivity.kt
+++ app/src/main/java/com/hackaprende/basketballscore/MainActivity.kt
@@ -2,10 +2,48 @@ package com.hackaprende.basketballscore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hackaprende.basketballscore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var localScore = 0
+    private var visitorScore = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val localScoreText = binding.localScoreText
+        val visitorScoreText = binding.visitorScoreText
+        binding.localMinusButton.setOnClickListener {
+            if (localScore > 0) {
+                localScore--
+                localScoreText.text = localScore.toString()
+            }
+        }
+
+        binding.localPlusButton.setOnClickListener {
+            localScore++
+            localScoreText.text = localScore.toString()
+        }
+
+        binding.visitorMinusButton.setOnClickListener {
+            if (visitorScore > 0) {
+                visitorScore--
+                visitorScoreText.text = visitorScore.toString()
+            }
+        }
+
+        binding.visitorPlusButton.setOnClickListener {
+            visitorScore++
+            visitorScoreText.text = visitorScore.toString()
+        }
+
+        binding.restartButton.setOnClickListener {
+            localScore = 0
+            visitorScore = 0
+            visitorScoreText.text = localScore.toString()
+            localScoreText.text = visitorScore.toString()
+        }
     }
 }
